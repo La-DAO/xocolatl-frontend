3014b7e133e05b66273e57fabfa8076892c31909
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-10-30 11:42:35 +0100

    Update i18n.ts
    Simple capitalizacion de los idiomas

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import { register, init, getLocaleFromNavigator } from "svelte-i18n";
 
-register("es", () => import("../public/static/locale/es.json"));
-register("en", () => import("../public/static/locale/en.json"));
+register("Es", () => import("../public/static/locale/es.json"));
+register("En", () => import("../public/static/locale/en.json"));
 
 init({
   fallbackLocale: "es",
